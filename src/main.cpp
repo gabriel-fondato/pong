90bb343e9a31568bf65e696050f47efe24d09c3b
red <gabrielfondato485@gmail.com>
2023-09-22 22:56:05 -0300

    no idea but now there is a square

--- src/main.cpp
+++ src/main.cpp
@@ -2,17 +2,62 @@
 #include "log.h"
 #include "main.h"
 
+void load() {
+    leftPaddle.x = 0;
+    leftPaddle.y = 0;
+    leftPaddle.w = 32;
+    leftPaddle.h = 32;
+    color.r = 255;
+    color.g = 255;
+    color.b = 255;
+
+
+}
+
+
+void update() {
+
+
+
+}
+
+void input() {
+    while (SDL_PollEvent(&event)) {
+        switch (event.type) {
+            case SDL_QUIT:
+                running = false;
+                break;
+        }
+    }
+
+
+}
+
+void draw() {
+    SDL_SetRenderDrawColor(renderer, 0x00, 0x00, 0x00, 255);
+    SDL_RenderClear(renderer);
+    SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b, 255);
+    SDL_RenderFillRect(renderer, &leftPaddle);
+    SDL_RenderPresent(renderer);
+
+}
+
 int main(int argc, char* args[]) {
+
     if(SDL_Init(SDL_INIT_EVERYTHING) < 0) 
-        log("Falied at SDL_Init()");
-    if(SDL_CreateWindowAndRenderer(WIDTH, HEIGHT,0, &window, &renderer) < 0)
-        log("Falied at SDL_CreateWindowAndRenderer()");
+        log("Falied at SDL_Init()"||SDL_GetError());
+    if(SDL_CreateWindowAndRenderer(WIDTH, HEIGHT,SDL_WINDOW_RESIZABLE, &window, &renderer) < 0)
+        log("Falied at SDL_CreateWindowAndRenderer()"||SDL_GetError());
+    SDL_SetWindowTitle(window,"Pong");
+    
     TTF_Init();
     font = TTF_OpenFont("comicsans.ttf", fontSize);
 
+    load();
     while(running) { //THE MAIN THING!!!!!
-
-
+        input();
+        update();
+        draw();
 
 
 
@@ -23,4 +68,3 @@ int main(int argc, char* args[]) {
     SDL_DestroyWindow(window);
     SDL_Quit();
 }
-
--- src/main.h
+++ src/main.h
@@ -4,8 +4,11 @@
 
 SDL_Renderer* renderer;
 SDL_Window* window;
+SDL_Surface *surface;
 TTF_Font *font;
 SDL_Color color;
+SDL_Event event;
+SDL_Rect leftPaddle, rightPaddle;
 bool running = true;
 const int WIDTH = 1024;
 const int HEIGHT = 768;
